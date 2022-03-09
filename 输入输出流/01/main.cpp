b1cdb5668f9ca7baece87f2bb0b5298a4216b61a
yangyan <yangyan@geek-zoo.com>
2022-03-09 14:54:40 +0800

    fix

--- 输入输出流/01/main.cpp
+++ 输入输出流/01/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
-//freopen
+//freopen 以write形式写入文件
 
 int main()
 {
-  int x,y;
-  cin >> x >> y;
-  freopen("test.txt", "w", stdout);
-  if (y == 0)
-  {
-    cerr << "error" << endl;
-  }
-  else
-  {
-    cout << x << " / " << y << " = " << x / y << endl;
-  }
-  return 0;
+  // int x,y;
+  // cin >> x >> y;
+  // freopen("test.txt", "w", stdout);
+  // if (y == 0)
+  // {
+  //   cerr << "error" << endl;
+  // }
+  // else
+  // {
+  //   cout << x << " % " << y << " = " << x % y << endl;
+  // }
+  // return 0;
+
+//freopen以read模式
+
+    char *x;
+    freopen("input.dat", "r", stdin); //标准输入定向到文件
+    while (cin.get() != EOF)
+    {
+      cin >> x;
+    };
+
+    while (cin.get() != EOF)
+    {
+      cout.put(cin.get());
+    }
+    
+    cout << x << endl;
+    return 0;
 }
 
