b06b3440b7309b71dccb26ee25cf77cf12b19376
yangyan <yangyan@geek-zoo.com>
2022-03-23 11:58:38 +0800

    读文本文件内容并输出

--- test-2020-08/test/42/main.cpp
+++ test-2020-08/test/42/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include <fstream> //extend from istream/ostream
 using namespace std;
 
@@ -7,23 +8,36 @@ using namespace std;
 int main()
 {
   // called stream's constructor func(..)
-    ifstream txtFile("1.txt"); //default open use in mode
-    string temp;
-    if (txtFile)
-    {
-      //iterate txtFile
-      while (getline(txtFile, temp))
-      {
-        /* code */
-        cout << temp << endl;
-      }
-    }
-    else
-    {
-      cout << "error in open file" << endl;
-    }
-/////////////////////////
+  // ifstream txtFile("1.txt"); //default open use in mode
+  // string temp;
+  // if (txtFile)
+  // {
+  //   //iterate txtFile
+  //   while (getline(txtFile, temp))
+  //   {
+  //     /* code */
+  //     cout << temp << endl;
+  //   }
+  // }
+  // else
+  // {
+  //   cout << "error in open file" << endl;
+  // }
 
+  // 参考http://c.biancheng.net/view/7609.html
+  /////////////////////////
+
+  char c[90];
+
+  ifstream inFile("in.txt", ios::in);
+
+  if (!inFile)
+    return 0;
+
+  while (inFile.getline(c, 90))
+  {
+    cout << c << endl;
+  }
 
   return 0;
 }
