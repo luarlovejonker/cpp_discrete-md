a37d69ab209cd020324c1b8a827e9cbdd86d126e
yangyan <1923882483@qq.com>
2022-04-11 20:46:29 +0800

    fix

--- test-2020-10/05/main.cpp
+++ test-2020-10/05/main.cpp
@@ -7,30 +7,28 @@ using namespace std;
 class Student
 {
 public:
-	//construct
-	Student()
-	{
-		cout << "Student de construct function call" << endl;
-	}
-	//disconstruct
-	~Student()
-	{
-		cout << "Student 的 disconstruct function call" << endl;
-	}
+//construct
+Student()
+{
+cout << "Student de construct function call" << endl;
+}
+//disconstruct
+~Student()
+{
+cout << "Student 的 disconstruct function call" << endl;
+}
 
 };
 
 void test01()
 {
-	Student *ss = new Student[2];
+Student *ss = new Student[2];
   delete []ss;
 }
 
 int main() {
-	
-	test01();
 
-	system("pause");
-
-	return 0;
+test01();
+system("pause");
+return 0;
 }
--- test-2020-10/06/main.cpp
+++ test-2020-10/06/main.cpp
@@ -1,13 +1,12 @@
 class Demo{
-  public: 
-    void SetValue (){
-
+  public:
+    void SetValue(){
     }
-    
-  };
+};
 int main() 
 {
-  const Demo Obj;
+  const Demo Obj; //const修饰的对象即为常量对象   
   Obj.SetValue();
   return 0;
 }
+//选A
